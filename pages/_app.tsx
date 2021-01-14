1e34f929e54ad2ddf9ce1d4935f23c9109e3dfd0
髙濱敦弘 <takahama1992@gmail.com>
2021-01-14 12:54:01 +0900

    add docker file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
 import { AppProps } from "next/app";
 
 const App = ({ Component, pageProps }: AppProps) => {
-  <> <Component {...pageProps} /> </>
+  return (
+    <Component {...pageProps} />
+  )
 }
 
 export default App
