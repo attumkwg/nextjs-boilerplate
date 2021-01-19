54c807e8076f516ff82e44feaf34d86787155250
髙濱敦弘 <takahama1992@gmail.com>
2021-01-19 14:11:47 +0900

    add debug mode

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,11 +2,11 @@ import Layout from '../components/Layout'
 import Button from '@material-ui/core/Button'
 import Router from 'next/router'
 
-const AboutPage = () => {
-  function goHome () {
-    Router.push('/')
-  }
+function goHome () {
+  Router.push('/')
+}
 
+const AboutPage = () => {
   return (
     <Layout title="About | Next.js + TypeScript Example">
       <h1>About</h1>
